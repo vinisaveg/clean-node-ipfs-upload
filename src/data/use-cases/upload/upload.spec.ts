1ea263fdb5c4f38efd98f9f27a900cc2370f99bb
savegnago <savegdesigner@gmail.com>
2022-07-22 19:25:51 -0300

    test: add upload use case test setup

---
+++ src/data/use-cases/upload/upload.spec.ts
@@ -0,0 +1,5 @@
+describe("Upload Use Case", () => {
+  it.todo("Should call Uploader with correct data");
+  it.todo("Should throw if Uploader throws");
+  it.todo("Should return correct data on Upload execution");
+});
