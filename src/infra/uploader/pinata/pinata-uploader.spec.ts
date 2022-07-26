62e5b34f34a14ffa82b00ce1fd00c2b399057135
savegnago <savegdesigner@gmail.com>
2022-07-26 18:55:31 -0300

    test: ensure to call PinataUploader execute with correct data

--- src/infra/uploader/pinata/pinata-uploader.spec.ts
+++ src/infra/uploader/pinata/pinata-uploader.spec.ts
@@ -1,4 +1,24 @@
+import { PinataUploader } from "./pinata-uploader";
+
+import { createReadStream } from "fs";
+import path from "path";
+
 describe("Pinata Uploader", () => {
-  it.todo("Should call execute with correct data");
+  it("Should call execute with correct data", async () => {
+    const sut = new PinataUploader();
+
+    const testFilePath = path.resolve("./test/fixtures/files/test-file.gif");
+    const testFile = createReadStream(testFilePath);
+    const uploaderParams = {
+      data: [testFile],
+    };
+
+    const pinataUploaderSpy = jest.spyOn(sut, "execute");
+
+    await sut.execute(uploaderParams);
+
+    expect(pinataUploaderSpy).toHaveBeenCalledWith(uploaderParams);
+  });
+
   it.todo("Should return correct uploaded data");
 });
