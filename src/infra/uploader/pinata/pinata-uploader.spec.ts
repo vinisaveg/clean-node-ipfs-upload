ef2af9fe97ed7b431027b1057c39193c1d7cb54c
savegnago <savegdesigner@gmail.com>
2022-08-02 19:13:52 -0300

    test: ensure to return correct uploaded data

--- src/infra/uploader/pinata/pinata-uploader.spec.ts
+++ src/infra/uploader/pinata/pinata-uploader.spec.ts
@@ -1,7 +1,20 @@
 import { PinataUploader } from "./pinata-uploader";
 
+import { PinataPinResponse } from "@pinata/sdk";
 import { createReadStream } from "fs";
 import path from "path";
+
+jest.mock("./utils/pinata-helper.ts", () => ({
+  PinataHelper: {
+    async upload(): Promise<PinataPinResponse> {
+      return {
+        IpfsHash: "IpfsHash",
+        PinSize: 100,
+        Timestamp: "Timestamp",
+      };
+    },
+  },
+}));
 
 describe("Pinata Uploader", () => {
   it("Should call execute with correct data", async () => {
@@ -20,5 +33,17 @@ describe("Pinata Uploader", () => {
     expect(pinataUploaderSpy).toHaveBeenCalledWith(uploaderParams);
   });
 
-  it.todo("Should return correct uploaded data");
+  it("Should return correct uploaded data", async () => {
+    const sut = new PinataUploader();
+
+    const testFilePath = path.resolve("./test/fixtures/files/test-file.gif");
+    const testFile = createReadStream(testFilePath);
+    const uploaderParams = {
+      data: [testFile],
+    };
+
+    const result = await sut.execute(uploaderParams);
+
+    expect(result[0]).toHaveProperty("cid", "IpfsHash");
+  });
 });
