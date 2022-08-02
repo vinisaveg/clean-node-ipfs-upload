c7a36010ffccd2dd4415a49b0c29529724eb69a7
savegnago <savegdesigner@gmail.com>
2022-08-02 19:13:29 -0300

    feat: ensure to return correct uploaded data

--- src/infra/uploader/pinata/pinata-uploader.ts
+++ src/infra/uploader/pinata/pinata-uploader.ts
@@ -1,15 +1,21 @@
-import { Uploader, UploaderResult } from "data/protocols/uploader";
-import { UploadParams } from "domain/use-cases/upload";
+import {
+  Uploader,
+  UploaderParams,
+  UploaderResult,
+} from "data/protocols/uploader";
+import { PinataHelper } from "./utils/pinata-helper";
 
 export class PinataUploader implements Uploader {
-  async execute(data: UploadParams): Promise<UploaderResult> {
+  async execute(params: UploaderParams): Promise<UploaderResult> {
+    const result = await PinataHelper.upload(params.data[0]);
+
     return Promise.resolve([
       {
-        name: "test",
-        size: 100,
-        extension: "test",
-        path: "test",
-        cid: "test",
+        name: "test-file",
+        size: result.PinSize,
+        extension: ".gif",
+        path: `ipfs.io/ipfs/${result.IpfsHash}`,
+        cid: result.IpfsHash,
       },
     ]);
   }
