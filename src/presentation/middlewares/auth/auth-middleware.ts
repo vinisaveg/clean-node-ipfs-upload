69677db28f895627ab92170d04f6fe3648b451a4
savegnago <savegdesigner@gmail.com>
2022-08-09 23:25:03 -0300

    feat: add AuthMiddleware

---
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -0,0 +1,36 @@
+import { InvalidTokenError } from "presentation/errors/invalid-token-error";
+import { ServerError } from "presentation/errors/server-error";
+import { HttpResponse } from "presentation/protocols/http";
+import { Middleware } from "presentation/protocols/middleware";
+import { Validation } from "presentation/protocols/validation";
+
+export class AuthMiddleware implements Middleware {
+  constructor(private readonly tokenValidation: Validation<string>) {}
+
+  handle(request: any): HttpResponse<any> {
+    try {
+      const apiToken = request.apiToken;
+
+      if (apiToken) {
+        const isValid = this.tokenValidation.validate(apiToken);
+
+        if (isValid) {
+          return {
+            statusCode: 200,
+            body: null,
+          };
+        }
+      }
+
+      return {
+        statusCode: 403,
+        body: new InvalidTokenError(),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(error as Error),
+      };
+    }
+  }
+}
