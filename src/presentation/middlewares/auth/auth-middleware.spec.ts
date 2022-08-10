f9080bc2a5bebfe8d0ad7cb5f801a2e713da7790
savegnago <savegdesigner@gmail.com>
2022-08-09 23:25:32 -0300

    test: add AuthMiddleware unit tests todo

---
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -0,0 +1,6 @@
+describe("AuthMiddleware", () => {
+  it.todo("Should call TokenValidation with correct value");
+  it.todo("Should return 200 if apiToken is valid");
+  it.todo("Should return an InvalidTokenError if apiToken is invalid");
+  it.todo("Should return 500 if TokenValidation throws");
+});
