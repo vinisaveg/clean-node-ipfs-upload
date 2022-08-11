3f89710f1ecd4eddd1de03d53853bbae69617744
savegnago <savegdesigner@gmail.com>
2022-08-11 19:58:21 -0300

    test: ensure to return 200 if apitToken is valid

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -16,7 +16,22 @@ describe("AuthMiddleware", () => {
 
     expect(tokenValidationSpy.token).toBe(request.apiToken);
   });
-  it.todo("Should return 200 if apiToken is valid");
+
+  it("Should return 200 if apiToken is valid", () => {
+    const tokenValidationSpy = new TokenValidationSpy();
+    const sut = new AuthMiddleware(tokenValidationSpy);
+
+    const request = {
+      apiToken: faker.random.alphaNumeric(16),
+    };
+
+    tokenValidationSpy.result = null;
+
+    const response = sut.handle(request);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe(null);
+  });
   it.todo("Should return an InvalidTokenError if apiToken is invalid");
   it.todo("Should return 500 if TokenValidation throws");
 });
