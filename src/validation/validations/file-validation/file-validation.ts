18d9db0a175b408e55e9e107d73491b899b611d8
savegnago <savegdesigner@gmail.com>
2022-08-10 22:23:05 -0300

    feat: ensure that fileValidator validates a file correctly

--- src/validation/validations/file-validation/file-validation.ts
+++ src/validation/validations/file-validation/file-validation.ts
@@ -1,8 +1,18 @@
 import { File } from "domain/entities/file";
+import { InvalidFileError } from "../../../presentation/errors/invalid-file-error";
 import { Validation } from "presentation/protocols/validation";
+import { FileValidator } from "validation/protocols/file-validator";
 
 export class FileValidation implements Validation<File> {
+  constructor(private readonly fileValidator: FileValidator) {}
+
   validate(input: File): Error | null {
+    const isValid = this.fileValidator.isValid(input);
+
+    if (!isValid) {
+      return new InvalidFileError();
+    }
+
     return null;
   }
 }
