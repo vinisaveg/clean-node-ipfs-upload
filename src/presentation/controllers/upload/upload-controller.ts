43c936a596cda80bccbf15a3e0a3b12105aae5bd
savegnago <savegdesigner@gmail.com>
2022-08-11 21:03:31 -0300

    feat: ensure to return 400 if request is invalid

--- src/presentation/controllers/upload/upload-controller.ts
+++ src/presentation/controllers/upload/upload-controller.ts
@@ -1,19 +1,38 @@
+import { File } from "domain/entities/file";
 import { Upload, UploadParams, UploadResult } from "domain/use-cases/upload";
 import { Controller } from "presentation/protocols/controller";
 import { HttpResponse } from "presentation/protocols/http";
+import { Validation } from "presentation/protocols/validation";
 
 export class UploadController
   implements Controller<UploadParams, UploadResult>
 {
-  constructor(private readonly upload: Upload) {}
+  constructor(
+    private readonly upload: Upload,
+    private readonly fileValidation: Validation<File>
+  ) {}
 
   async handle(request: UploadParams): Promise<HttpResponse<UploadResult>> {
     try {
-      const uploadResult = await this.upload.execute(request);
+      const error = this.fileValidation.validate(request.data[0]);
+
+      if (error === null) {
+        const uploadResult = await this.upload.execute(request);
+
+        return {
+          statusCode: 200,
+          body: uploadResult,
+        };
+      }
 
       return {
-        statusCode: 200,
-        body: uploadResult,
+        statusCode: 400,
+        body: {
+          error: {
+            name: error.name,
+            message: error.message,
+          },
+        },
       };
     } catch (error: any) {
       return {
