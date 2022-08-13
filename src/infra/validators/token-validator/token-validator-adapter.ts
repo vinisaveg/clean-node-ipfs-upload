462618c17452e75170c011f17a62d0c45c9f4bef
savegnago <savegdesigner@gmail.com>
2022-08-13 11:08:26 -0300

    feat: ensure to return false if token is invalid

--- src/infra/validators/token-validator/token-validator-adapter.ts
+++ src/infra/validators/token-validator/token-validator-adapter.ts
@@ -1,7 +1,22 @@
 import { TokenValidator } from "validation/protocols/token-validator";
 
+import jwt from "jsonwebtoken";
+
 export class TokenValidatorAdapter implements TokenValidator {
   isValid(token: string): boolean {
-    return false;
+    try {
+      const decodedToken = jwt.verify(
+        token,
+        process.env.JWT_SECRET as string
+      ) as any;
+
+      if (decodedToken.pass === process.env.APP_PASS) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      return false;
+    }
   }
 }
