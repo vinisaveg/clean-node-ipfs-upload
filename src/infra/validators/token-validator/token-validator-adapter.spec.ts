0616984ac40cfd5199d2d2895a5c457fff6c1e60
savegnago <savegdesigner@gmail.com>
2022-08-13 11:09:54 -0300

    test: ensure to return false if token is invalid

--- src/infra/validators/token-validator/token-validator-adapter.spec.ts
+++ src/infra/validators/token-validator/token-validator-adapter.spec.ts
@@ -1,18 +1,38 @@
 import { faker } from "@faker-js/faker";
 import { TokenValidatorAdapter } from "./token-validator-adapter";
 
+import jwt from "jsonwebtoken";
+
+jest.mock("jsonwebtoken", () => ({
+  verify(): any {
+    return {
+      pass: "pass",
+    };
+  },
+}));
+
 describe("TokenValidatorAdapter", () => {
   it("Should validate with correct value", () => {
     const sut = new TokenValidatorAdapter();
 
     const token = faker.random.alphaNumeric(16);
 
-    jest.spyOn(sut, "isValid");
+    const jwtVerifySpy = jest.spyOn(jwt, "verify");
 
     sut.isValid(token);
 
-    expect(sut.isValid).toHaveBeenCalledWith(token);
+    expect(jwtVerifySpy).toHaveBeenCalledWith(token, "secret");
   });
-  it.todo("Should return false if token is invalid");
+
+  it("Should return false if token is invalid", () => {
+    const sut = new TokenValidatorAdapter();
+
+    const invalidtoken = faker.random.alphaNumeric(16);
+
+    const result = sut.isValid(invalidtoken);
+
+    expect(result).toBe(false);
+  });
+
   it.todo("Should return true if token is valid");
 });
