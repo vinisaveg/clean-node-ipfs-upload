31018baed6f4a206bef3c316be045f0335511c95
savegnago <savegdesigner@gmail.com>
2022-08-13 09:10:32 -0300

    feat: ensure to return false if file is invalid

--- src/infra/validators/file-validator/file-validator-adapter.ts
+++ src/infra/validators/file-validator/file-validator-adapter.ts
@@ -2,7 +2,32 @@ import { File } from "domain/entities/file";
 import { FileValidator } from "validation/protocols/file-validator";
 
 export class FileValidatorAdapter implements FileValidator {
+  validExtensions: Array<string> = [".png", ".gif"];
+
   isValid(file: File): boolean {
+    const isEmpty = this.isEmpty(file.name, file.extension);
+    const isExtensionInvalid = this.isExtensionInvalid(file.extension);
+
+    if (isEmpty || isExtensionInvalid) {
+      return false;
+    }
+
+    return true;
+  }
+
+  isEmpty(name: string, extension: string): boolean {
+    if (name === "" || extension === "") {
+      return true;
+    }
+
     return false;
+  }
+
+  isExtensionInvalid(extension: string): boolean {
+    if (this.validExtensions.includes(extension)) {
+      return false;
+    }
+
+    return true;
   }
 }
