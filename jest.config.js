f8a8f0403bb688e1f3cabb60bf185a8de2690d2d
savegnago <savegdesigner@gmail.com>
2022-08-13 15:11:33 -0300

    chore: add remove unneeded files to jest coverage

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,10 @@ module.exports = {
   collectCoverageFrom: [
     "<rootDir>/src/**/*.ts",
     "!<rootDir>/src/infra/uploader/pinata/utils/pinata-helper.ts",
+    "!<rootDir>/src/main/adapters/**/*.ts",
+    "!<rootDir>/src/main/factories/**/*.ts",
+    "!<rootDir>/src/main/server/server.ts",
+    "!<rootDir>/src/main/index.ts",
   ],
   coverageDirectory: "coverage",
 };
