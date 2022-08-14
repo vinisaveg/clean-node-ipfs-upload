bb1caf3920b46ce94862a4590b4c4a22d819de76
savegnago <savegdesigner@gmail.com>
2022-08-14 10:06:37 -0300

    fix: change file buffer to a readStream

--- src/main/adapters/multer/multer-adapter.ts
+++ src/main/adapters/multer/multer-adapter.ts
@@ -1,22 +1,29 @@
 import { UploadParams, UploadResult } from "../../../domain/use-cases/upload";
-import { Request, Response } from "express";
 import { Controller } from "../../../presentation/protocols/controller";
+
+import { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
 
 export const multerAdapter = (
   controller: Controller<UploadParams, UploadResult>
 ) => {
   return async (request: Request, response: Response) => {
-    const files = request.files as Array<Express.Multer.File>;
-    const fileName = files[0].originalname.split(".");
-    const extension = fileName[1];
+    const file = request.file as Express.Multer.File;
+    const originalname = file.originalname.split(".");
+    const filename = originalname[0];
+    const extension = originalname[1];
+
+    const filePath = path.resolve(`./uploads/${file.filename}`);
+    const fileReadstream = fs.createReadStream(filePath);
 
     const requestData: UploadParams = {
       data: [
         {
-          buffer: files[0].buffer,
-          name: fileName[0],
-          size: files[0].size,
+          name: filename,
+          size: file.size,
           extension: `.${extension}`,
+          stream: fileReadstream,
         },
       ],
     };
